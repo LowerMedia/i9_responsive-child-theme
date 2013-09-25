809bfd2469e9fb04439134fdb42fedc1a962561e
Pete Lower <pete.lower@gmail.com>
2013-09-25 13:44:29 -0400

    adding alexa info meta tag

--- header.php
+++ header.php
@@ -28,7 +28,7 @@ if ( !defined('ABSPATH')) exit;
 <link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
 <meta charset="<?php bloginfo('charset'); ?>" />
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-
+<meta name="alexaVerifyID" content="oIrBD5UAoK17QL2EVnnV7IQk2bc" />
 <title><?php wp_title('&#124;', true, 'right'); ?><?php bloginfo('name'); ?></title>
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
